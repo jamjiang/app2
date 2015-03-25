cda2607884278a2563e9127bc72d9db0a587686c
jamjiang <jiang_xing@hotmail.com>
2015-03-25 16:10:35 +0800

    first try

---
+++ Break.py
@@ -0,0 +1,7 @@
+__author__ = 'jamjiang'
+while True:
+    s=raw_input('enter somthing<enter quit to exit>:')
+    if s== 'quit':
+        break
+    print('Length of the straing is',len(s))
+print 'done'
